ab142e1f434f482cac01698537966a51c6e1e415
Parthvsquare <parthvsquare@gmail.com>
2021-07-28 20:12:12 +0530

    boiler plate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
+import Header from './Header/Header';
+import ItemCard from './ItemCard/ItemCard';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <ItemCard/>
     </div>
   );
 }
---
+++ src/ItemCard/ItemCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './ItemCard.module.css'
+
+
+function ItemCard() {
+    return (
+        <div className="styles.itemCard">
+            
+        </div>
+    )
+}
+
+export default ItemCard
