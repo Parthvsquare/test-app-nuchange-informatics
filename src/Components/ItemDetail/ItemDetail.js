6ff9f0fa88052d0f0a45846c3d9f1a50df7f4137
Parthvsquare <parthvsquare@gmail.com>
2021-07-28 20:17:57 +0530

    added boilerplate

---
+++ src/Components/ItemDetail/ItemDetail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './ItemDetail.module.css'
+
+function ItemDetail() {
+    return (
+        <div className="styles.itemDetail">
+            
+        </div>
+    )
+}
+
+export default ItemDetail
